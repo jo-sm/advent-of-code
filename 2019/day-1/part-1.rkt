6c9c06efb526deeacfd01f1ddee44e862fa07207
Joshua Smock <jo-sm@users.noreply.github.com>
2020-12-05 17:19:28 +0100

    Simplify 2019 day 1

--- 2019/day-1/part-1.rkt
+++ 2019/day-1/part-1.rkt
@@ -1,29 +1,9 @@
 #lang racket
 
-(define (read-fuel-input)
-  (define io (open-input-file "fuel-input.txt" #:mode 'text))
+(require "../utils.rkt")
 
-  ; Iterate over each line until eof. For each line, convert the read string into a number
-  ; Since order isn't important for this exercise, it is reverse of the order in `fuel-input.txt`
-  (define (iter i)
-    (let ([line (read-line io)])
-      (if (equal? eof line)
-        i
-        (iter (cons (string->number line) i))
-      )
-    )
-  )
-
-  (iter '())
+(define (mass-fuel-req mass)
+  (- (floor (/ mass 3)) 2)
 )
 
-(define (calc-fuel-req-for-mass mass) (- (floor (/ mass 3)) 2))
-
-(define (sum-fuel-reqs)
-  ; Add each fuel requirement together until there is nothing left in the list
-  ; Fuel req. calc: f(mass) = [[mass / 3]] - 2
-
-  (foldr (lambda (i memo) (+ memo (calc-fuel-req-for-mass i))) 0 (read-fuel-input))
-)
-
-(sum-fuel-reqs)
+(apply + (map mass-fuel-req (read "fuel-input.txt" string->number)))
--- 2019/day-1/part-2.rkt
+++ 2019/day-1/part-2.rkt
@@ -1,34 +1,18 @@
 #lang racket
 
-(define (read-fuel-input)
-  (define io (open-input-file "fuel-input.txt" #:mode 'text))
+(require "../utils.rkt")
 
-  (define (iter i)
-    (let ([line (read-line io)])
-      (if (equal? eof line)
-        i
-        (iter (cons (string->number line) i))
+(define (module-fuel-req mass)
+  (define (iter total rem-mass)
+    (let ([next (- (floor (/ rem-mass 3)) 2)])
+      (if (<= next 0)
+        total
+        (iter (+ total next) next)
       )
     )
   )
 
-  (iter '())
+  (iter 0 mass)
 )
 
-(define (calc-fuel-req-for-module total mass)
-  ; If the calculation is less than 0, do not add it to the result, and return
-  ; the result. Otherwise, add it and continue the iteration.
-  ; Fuel req calc: [[i/3]] - 2
-  (let ([next (- (floor (/ mass 3)) 2)])
-    (if (<= next 0)
-      total
-      (calc-fuel-req-for-module (+ total next) next)
-    )
-  )
-)
-
-(define (sum-fuel-reqs)
-  (foldr (lambda (i memo) (+ memo (calc-fuel-req-for-module 0 i))) 0 (read-fuel-input))
-)
-
-(sum-fuel-reqs)
+(apply + (map module-fuel-req (read "fuel-input.txt" string->number)))
