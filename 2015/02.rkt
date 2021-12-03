bdb1c1332d467c06c8592b4b6360b936154be07f
Joshua Smock <jo-sm@users.noreply.github.com>
2021-12-03 10:08:07 +0100

    2015 day 2

---
+++ 2015/02.rkt
@@ -0,0 +1,41 @@
+#lang racket/base
+
+(require "./utils.rkt"
+         threading
+         racket/string
+         racket/list
+         rackunit
+         srfi/26)
+
+(define (smallest-number numbers)
+  (for/fold ([result +inf.0]) ([num numbers])
+    (if (< num result) num result)))
+
+(define (dimensions->sides dimensions)
+  (~>> (string-split dimensions "x") (map string->number)))
+
+(define (dimensions->sqft dimensions)
+  (define surface-areas (~>> dimensions dimensions->sides (combinations _ 2) (map (cut apply * <>))))
+  (define smallest (smallest-number surface-areas))
+
+  (~> surface-areas (map (lambda (side) (* 2 side)) _) (apply + _) (+ smallest)))
+
+(define (dimensions->ribbon-length dimensions)
+  (define sides (~> (dimensions->sides dimensions) (sort <)))
+
+  (+ (* 2 (car sides)) (* 2 (cadr sides)) (apply * sides)))
+
+(define (part-1 input)
+  (~>> input (map dimensions->sqft) (apply +)))
+
+(define (part-2 input)
+  (~>> input (map dimensions->ribbon-length) (apply +)))
+
+(define example '("2x3x4" "1x1x10"))
+(define input (read-input-lines "02.rktd"))
+
+(check-eq? (part-1 example) (+ 58 43))
+(check-eq? (part-1 input) 1588178)
+
+(check-eq? (part-2 example) (+ 34 14))
+(check-eq? (part-2 input) 3783758)
--- 2015/utils.rkt
+++ 2015/utils.rkt
@@ -2,10 +2,17 @@
 
 (require racket/contract
          racket/function
-         racket/port)
+         racket/port
+         srfi/26
+         racket/string)
 
 (provide (all-defined-out))
 
 (define/contract (read-input-file [filename "input"] #:file-parser [file-parser identity])
                  (->* () (string? #:file-parser (-> string? any/c)) any/c)
                  (file-parser (port->string (open-input-file filename #:mode 'text))))
+
+(define/contract
+ (read-input-lines [filename "input"] #:line-parser [line-parser identity])
+ (->* () (string? #:line-parser (-> string? any/c)) any/c)
+ (map line-parser (read-input-file filename #:file-parser (cut string-split <> "\n"))))
